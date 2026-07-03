fbdcb528f6c756532a2fb41a96a8002f040e1f46
zcpefaisal <inboxlions@gmail.com>
2026-07-03 19:36:23 +0600

    feat: dockerize fastapi app and setup multi-container environment with docker-compose

--- app/core/config.py
+++ app/core/config.py
@@ -2,20 +2,19 @@ from pydantic_settings import BaseSettings,SettingsConfigDict
 from pydantic import computed_field
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: str
-    POSTGRES_DB: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str = "localhost"
+    DATABASE_PORT: int = 5432
+    DATABASE_NAME: str
 
     # Using computed_field to dynamically create DATABASE_URL
     @computed_field
     @property
     def DATABASE_URL(self) -> str:
-        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-    
+        return f"postgresql://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
     # Describe to Pydantic where the .env file is located
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    
 # Creating a global object, which will be imported and used throughout the project
 settings = Settings()
